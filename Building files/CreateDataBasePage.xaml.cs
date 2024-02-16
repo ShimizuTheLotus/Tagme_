6e4b51350e5d44f32a1eea72b0c3f7d265e38698
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2024-02-16 22:41:04 +0800

    Update

--- Building files/CreateDataBasePage.xaml.cs
+++ Building files/CreateDataBasePage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -27,12 +28,41 @@ namespace Tagme_
             this.InitializeComponent();
 
             Loaded += CreateDataBasePage_Loaded;
+            SizeChanged += CreateDataBasePage_SizeChanged;
         }
+
 
         //Loaded
         private void CreateDataBasePage_Loaded(object sender, RoutedEventArgs e)
         {
             InitializeShadow();
+        }
+
+        //SizeChanged
+        private void CreateDataBasePage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            FitLayout();
+        }
+        private void FitLayout()
+        {
+            if (DataBaseCoverImage.ActualWidth / BasePanel.ActualWidth < 0.4)
+            {
+                EditInfoPanel.Orientation = Orientation.Horizontal;
+                DataBaseNameTextBox.Width = BasePanel.ActualWidth 
+                    - DataBaseCoverPanel.ActualWidth 
+                    - DataBaseCoverPanel.Margin.Left 
+                    - DataBaseCoverPanel.Margin.Right
+                    - MovableSettingsPanel.Margin.Left
+                    - MovableSettingsPanel.Margin.Right
+                    - DataBaseNameTextBox.Margin.Left
+                    - DataBaseNameTextBox.Margin.Right;
+            }
+            else
+            {
+                EditInfoPanel.Orientation = Orientation.Vertical;
+                DataBaseNameTextBox.Width = BasePanel.ActualWidth - 2 * DataBaseNameTextBox.Margin.Left;
+            }
         }
 
         //Initialize UI shadow
