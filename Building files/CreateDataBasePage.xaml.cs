843cce19e8da6be4ffdb5726ffff54adb0b8bcd2
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2024-02-23 13:55:50 +0800

    Update

--- Building files/CreateDataBasePage.xaml.cs
+++ Building files/CreateDataBasePage.xaml.cs
@@ -72,7 +72,7 @@ namespace Tagme_
         public void InitializeShadow()
         {
             DataBaseCoverPanel.Translation += new System.Numerics.Vector3(0, 0, 16);
-            CreateButtonPanel.Translation += new System.Numerics.Vector3(0, 0, 16);
+            CreateButtonPanel.Translation += new System.Numerics.Vector3(0, 0, 64);
         }
 
 
