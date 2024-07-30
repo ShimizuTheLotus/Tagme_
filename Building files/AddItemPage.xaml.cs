a1f2f19a638228d83294c83721e92fb51581efaf
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2024-07-30 17:18:31 +0800

    Update

--- Building files/AddItemPage.xaml.cs
+++ Building files/AddItemPage.xaml.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using CommunityToolkit.WinUI.Controls;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Xml.Linq;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,19 +28,88 @@ namespace Tagme_
         public AddItemPage()
         {
             this.InitializeComponent();
+
+            SizeChanged += AddItemPage_SizeChanged;
+            Loaded += AddItemPage_Loaded;
         }
 
-        public class TagListTemplate
+        private void AddItemPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            pageLoaded = true;
+        }
+
+        private void AddItemPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (pageLoaded)
+            {
+                /*DependencyObject startNode = TagTextBox.ItemsPanelRoot;
+                int count = VisualTreeHelper.GetChildrenCount(startNode);
+                for (int i = 0; i < count; i++)
+                {
+                    var child = VisualTreeHelper.GetChild(startNode, i);
+                    TokenizingTextBoxItem item = TagTextBox.ContainerFromItem(child) as TokenizingTextBoxItem;
+                    if (item != null)
+                    {
+                        item.MaxWidth = TagTextBox.ActualWidth;
+                    }
+                }
+                foreach (TokenizingTextBoxItem item in TagTextBox.ItemsPanelRoot.Children)
+                {
+                    item.Width = TagTextBox.ActualWidth;
+                    DependencyObject startNode = item;
+                    int count = VisualTreeHelper.GetChildrenCount(startNode);
+                    for (int i = 0; i < count; i++)
+                    {
+                        var child = VisualTreeHelper.GetChild(startNode, i);
+                        TokenizingTextBoxItem item2 = TagTextBox.ContainerFromItem(child) as TokenizingTextBoxItem;
+                        if (child is Grid)
+                        {
+                            Grid grid = child as Grid;
+                            grid.MaxWidth = TagTextBox.ActualWidth;
+                        }
+                        if (child is TextBlock)
+                        {
+                            TextBlock textBlock = child as TextBlock;
+                            textBlock.MaxWidth = TagTextBox.ActualWidth;
+                        }
+                        if (item2 != null)
+                        {
+                            item2.MaxWidth = TagTextBox.ActualWidth;
+                        }
+                    }
+
+                }*/
+            }
+        }
+
+        //status
+        bool pageLoaded { get; set; } = false;
+
+        //Variables
+        public ObservableCollection<string> Tags { get; set; } = new ObservableCollection<string>();
+        
+
+        public class TagListTemplateItem
         {
             public string Tag { get; set; }
         }
 
         public static class Data
         {
-            public static List<TagListTemplate> TagList = new List<TagListTemplate>();
+            public static List<TagListTemplateItem> TagList = new List<TagListTemplateItem>();
+        }
+        
+
+        private void TagTextBox_TokenItemAdding(CommunityToolkit.WinUI.Controls.TokenizingTextBox sender, CommunityToolkit.WinUI.Controls.TokenItemAddingEventArgs args)
+        {
+            //Avoid Tag repeating
+            if (!(args.Item == null && !Tags.Contains(args.TokenText)))
+            {
+                args.Cancel = true;
+            }
         }
 
-        private void TagTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void TagTextBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         {
 
         }
