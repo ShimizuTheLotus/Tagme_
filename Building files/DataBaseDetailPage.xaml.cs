30951d6978c22cd83366d42a5e1c255a1d3e5279
ShimizuTheLotus <152269217+ShimizuTheLotus@users.noreply.github.com>
2024-08-06 15:59:40 +0800

    Update

--- Building files/DataBaseDetailPage.xaml.cs
+++ Building files/DataBaseDetailPage.xaml.cs
@@ -23,6 +23,7 @@ namespace Tagme_
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
+    /// 
     public sealed partial class DataBaseDetailPage : Page
     {
         public DataBaseDetailPage()
